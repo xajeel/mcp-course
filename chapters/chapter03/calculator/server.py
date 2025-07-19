0581b90aba42883cd131900a089a486492340a76
xajeel <sajeel135@gmail.com>
2025-07-19 02:24:29 -0700

    calculator mcp server

---
+++ chapters/chapter03/calculator/server.py
@@ -0,0 +1,27 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("mcp-course")
+
+def output_formator(number: int):
+    return f"the asnwer is {number}"
+
+
+@mcp.tool()
+def add(a:int, b:int) -> str:
+    ans = a + b
+    return output_formator(ans)
+    
+@mcp.tool()
+def subtract(a:int, b:int) -> str:
+    ans = a - b
+    return output_formator(ans)
+
+@mcp.tool()
+def multiply(a:int, b:int) -> str:
+    ans = a * b
+    return output_formator(ans)
+
+@mcp.tool()
+def devide(a:int, b:int) -> str:
+    ans = a // b
+    return output_formator(ans)
