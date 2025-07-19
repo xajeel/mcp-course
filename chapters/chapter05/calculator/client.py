360e05a377f4b66b636ef74d305687d03864ef6a
xajeel <sajeel135@gmail.com>
2025-07-19 03:56:00 -0700

    calculator mcp client

---
+++ chapters/chapter05/calculator/client.py
@@ -0,0 +1,17 @@
+from mcp_use import MCPClient
+
+def make_client() -> MCPClient:
+
+    config = {
+        "mcpServers": {
+            "calculator": {
+                "command": "uv",
+                "args": ["run", "server.py"],
+                "env": {
+                    "PYTHONUNBUFFERED": "1"
+                }
+            }
+        }
+    }
+
+    return MCPClient.from_dict(config)
