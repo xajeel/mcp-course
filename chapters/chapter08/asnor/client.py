6906bde997f02eeb9ae585d5da45531e5ae15244
xajeel <sajeel135@gmail.com>
2025-07-23 10:39:44 -0700

    File Management MCP Client

---
+++ chapters/chapter08/asnor/client.py
@@ -0,0 +1,15 @@
+from mcp_use import MCPClient
+
+def make_client() -> MCPClient:
+    config = {
+        "mcpServers": {
+            "mcp01": {
+                "command": "uv",
+                "args": ["run", "server.py"],
+                "env": {
+                    "PYTHONUNBUFFERED": "1"
+                }
+            }
+        }
+    }
+    return MCPClient.from_dict(config)
